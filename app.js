f691d7b2fccc2908d83eda54d3e9c2deffe4c8a7
Rajvir Singh <rsaggu99@gmail.com>
2019-08-05 14:51:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require ('http');
+const nodeinfo = require('nodejs-info');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(nodeinfo(req));
+});
+
+server.listen(8080);
